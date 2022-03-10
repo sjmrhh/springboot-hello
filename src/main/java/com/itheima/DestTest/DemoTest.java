3c60ead919a51e927de7897f958c2c3d8599ce77
itcast <hello@itcast.cn>
2022-03-10 12:28:57 +0800

    1233333

--- src/main/java/com/itheima/DestTest/DemoTest.java
+++ src/main/java/com/itheima/DestTest/DemoTest.java
@@ -3,6 +3,7 @@ package com.itheima.DestTest;
 public class DemoTest {
     public String say(String name){
         System.out.println("hello "+name);
+        System.out.println("hello "+name);
         return "hello "+name;
     }
 }
